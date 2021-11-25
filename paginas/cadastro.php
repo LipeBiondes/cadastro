6ac9ea731610fd0e557944b2c1788babc7dc5d4c
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-11-25 20:10:45 -0300

    paginação

---
+++ paginas/cadastro.php
@@ -0,0 +1,19 @@
+ 
+    <h1 class="cadastro">CADASTRO</h1>
+    <!--formulário-->
+    <form action="">
+      <label for="login">Login</label>
+      <input type="text" id="nomesobrenome" class="input-padrao" required placeholder="examplename">
+
+      <label for="email">Email</label>
+      <input type="email" id="email" class="input-padrao" required placeholder="exemplo@gmail.com">
+
+      <label for="Telefone">Senha</label>
+      <input type="password" id="Telefone" class="input-padrao" required placeholder="8 a 12 caracteres">
+
+      <input type="submit" value="Cadastrar" class="enviar">
+      <a href="?pagina=login"> <button type="button" class="enviar">Voltar</button></a>
+
+
+    </form>
+ 
---
+++ paginas/login.php
@@ -0,0 +1,16 @@
+ 
+    <h1 class="cadastro">Login</h1>
+    <!--formulário-->
+    <form action="">
+      <label for="login">Login</label>
+      <input type="text" id="login" class="input-padrao" required placeholder="Seu login aqui">
+
+      <label for="senha">Senha</label>
+      <input type="password" id="Telefone" class="input-padrao" required placeholder="Sua senha aqui">
+
+      <input type="submit" value="Entrar" class="enviar">
+      <a href="?pagina=cadastro"><button type="button" class="enviar">Cadastre-se</button></a>
+
+    </form>
+
+ 
